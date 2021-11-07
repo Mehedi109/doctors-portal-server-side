f78496ebb048700d2a945d14544777a70880e1e0
Mehedi <mehedialam109@gmail.com>
2021-11-07 15:02:04 +0600

    get and post method of appointments added

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+const { MongoClient } = require('mongodb');
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8ppmn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    console.log('db connected');
+
+    const database = client.db('doctors-portal');
+    const appointmentsCollection = database.collection('appointments');
+
+    app.get('/appointments', async (req, res) => {
+      const email = req.query.email;
+      const date=new Date(req.query.date).toLocaleDateString();
+      const query = { email: email,date:date };
+      const cursor = appointmentsCollection.find(query);
+      const appointments = await cursor.toArray();
+      res.json(appointments);
+    });
+
+    app.post('/appointments', async (req, res) => {
+      const appointment = req.body;
+      const result = await appointmentsCollection.insertOne(appointment);
+      console.log(appointment);
+      res.json({ message: 'appoint' });
+    });
+  } finally {
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('Doctors Portal!');
+});
+
+app.listen(port, () => {
+  console.log(`listening at ${port}`);
+});
+
+// app.get('/users');
+// app.post('/users');
+// app.get('/users/:id');
+// app.put('/users/:id');
+// app.delete('/users/:id');
+// users: get;
+// users: post;
