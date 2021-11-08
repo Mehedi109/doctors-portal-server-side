98c09404e88de3488663c17c5e5f4216da1fb1eb
Mehedi <mehedialam109@gmail.com>
2021-11-08 21:43:07 +0600

    env file updated

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const { MongoClient } = require('mongodb');
 
 // doctors-portal-firebase-adminsdk.json
 
-const serviceAccount = require(`./doctors-portal-firebase-adminsdk.json`);
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
